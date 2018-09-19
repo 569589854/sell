bda998519ce48bbd30743b3fa71971546a4b8e74
Young程哥 <sun569589854@163.com>
2018-09-19 16:14:31 +0800

    mysell all done

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import goods from '@/components/goods/goods'
 import ratings from '@/components/ratings/ratings'
 import seller from '@/components/seller/seller'
-import test from '@/components/header/test'
 
 Vue.use(Router)
 
